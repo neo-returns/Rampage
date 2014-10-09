ce52cc19fe6ef2d34647c43cedc66505ff283b7f
Paul Taylor <paultaylor@PaulTaylor.local>
2014-10-09 16:25:19 -0500

    fixed tests

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,12 +5,12 @@
 angular.module('rampage.controllers', ['rampage.services'])
 .controller('MyCtrl1', function MyCtrl1($scope, kinvey) {
 
-	/*kinvey.ping().then(function(data) {
+	kinvey.ping().then(function(data) {
 		$scope.data = data.data;
 		$scope.status = data.status;
 	}, function(data) {
 		$scope.data = data || "Request failed";
-	});*/
+	});
 	
 }).controller('MyCtrl2', function MyCtrl2() {
 }).controller('TaskController', function TaskController($scope, kinvey, $modal, $q) {
