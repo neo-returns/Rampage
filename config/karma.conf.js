e02d2888988c97486cbd1756e54abca3757b8a28
Paul Taylor <paultaylor@PaulTaylor.local>
2014-10-09 14:47:34 -0500

    Added Threshold and Coverage

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,23 +1,66 @@
-basePath = '../';
+module.exports = function(config){
+    config.set({
+    basePath : '../',
 
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'test/lib/jQuery.js',
-  'app/lib/angular/angular.js',
-  'app/lib/angular/angular-*.js',
-  'test/lib/angular/angular-mocks.js',
-  'test/lib/bootstrap.js',
-  'test/lib/angular-strap.js',
-  'app/js/**/*.js',
-  'test/unit/**/*.js'
-];
+    files : [
+      'test/lib/jQuery.js',
+	  'app/lib/angular/angular.js',
+	  'app/lib/angular/angular-*.js',
+	  'test/lib/angular/angular-mocks.js',
+	  'test/lib/bootstrap.js',
+	  'test/lib/angular-strap.js',
+	  'app/js/**/*.js',
+	  'test/unit/**/*.js'
+    ],
 
-autoWatch = true;
+    exclude : [
+      'app/lib/angular/angular-loader.js',
+      'app/lib/angular/*.min.js',
+      'app/lib/angular/angular-scenario.js'
+    ],
 
-browsers = ['Chrome'];
+    autoWatch : true,
 
-junitReporter = {
-  outputFile: 'test_out/unit.xml',
-  suite: 'unit'
-};
+    frameworks: ['jasmine'],
+
+    browsers : ['PhantomJS'],
+
+    plugins : [
+            'karma-junit-reporter',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine',
+            'karma-phantomjs-launcher',
+            'karma-threshold-reporter',
+            'karma-coverage'
+    ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    },
+    
+    reporters: ['progress','coverage', 'threshold'],
+    
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'app/js/**/*.js': ['coverage']
+    },
+  		
+    // the configure thresholds
+    thresholdReporter: {
+      statements: 80,
+      branches: 30,
+      functions: 50,
+      lines: 80
+    },
+    
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    }
+
+})}
